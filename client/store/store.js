165f04abd943a36d9e8a9f795e47f3f9b5bc07a8
Lina Jones <lina.jones630@gmail.com>
2017-07-11 13:55:12 -0600

    breaking up reducers

--- client/store/store.js
+++ client/store/store.js
@@ -120,28 +120,7 @@ export function postChannel (channel, history) {
 
 // REDUCER
 
-/**
- * Whoa! What is this { ...state } business?!?
- * This is the spread operator like we've seen before - but this time, we're using it with an Object!
- * When we use the spread operator on an object, it extracts all of the key-value pairs on that object into a new object!
- * Sound familiar? It acts like Object.assign!
- *
- * For example:
- *
- *    const obj1 = { a: 1 };
- *    const obj2 = { ...obj1, b: 2  }
- *    console.log(obj2) // { a: 1, b: 2 }
- *
- * This is the same result we would have gotten if we had said:
- *
- *    const obj2 = Object.assign({}, obj1, { b: 2 })
- *
- * However, it's much less verbose!
- * Is there anything the spread operator DOESN'T do?!?
- *
- * Note: this is still an experimental language feature (though it is on its way to becoming official).
- * We can use it now because we are using a special babel plugin with webpack (babel-preset-stage-2)!
- */
+
 function reducer (state = initialState, action) {
 
   switch (action.type) {
